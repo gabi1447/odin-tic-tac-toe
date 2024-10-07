f2998b28e47e72f27c1baa61066edef0644e79c5
Gabriel <gabrielpachecotec@gmail.com>
2024-10-07 19:37:48 +0100

    Start Game module pattern
    In this module the logic of the tic-tac-toe game is going to be written.
    As of now we've written the beginning of the game loop, that is gonna run
    until the game has declared a winner.
    
    So far we've register the names of both players, randomized which player starts
    with the whoStartsGame(), captured the position in which the currentPlayer wants
    to place its marker and checked with the isBoardCellFilled() if that position is
    already filled.

--- scripts/script.js
+++ scripts/script.js
@@ -57,6 +57,51 @@ const GameBoard = (function() {
     }
 })();
 
+const Game = (function() {
+    let currentPlayerTurn;
+    
+    function start() {
+        const player1 = new Player(prompt("Name of player1:"));
+        const player2 = new Player(prompt("Name of player2:"));
+        const players = [player1, player2];
+
+        currentPlayerTurn = whoStartsGame(players);
+        console.log(GameBoard.printBoardState());
+
+        // GAME LOOP
+        while (true) {
+            console.log(`It's ${currentPlayerTurn.name} playing with the ${currentPlayerTurn.marker} marker.`);
+            const selectedPosition = prompt(`Provide a position (e.g. 02 representing the row and column) for your ${currentPlayerTurn.marker} marker`);
+
+            if (!isBoardCellFilled(selectedPosition)) {
+                GameBoard.addMarkerToBoardCell(selectedPosition, currentPlayerTurn.marker);
+            } else {
+                console.log("This board cell is already filled, try again.");
+                continue;
+            }
+
+            // Check winner combinations
+            
+        }
+    }
+
+    function isBoardCellFilled(selectedPosition) {
+        const [row, col] = selectedPosition.split("");
+        const board = GameBoard.getBoardState();
+
+        return board[+row][+col] === 0 ? false : true;
+    }
+
+    function whoStartsGame(arrayObjectPlayers) {
+        const randomIndex = GeneralModule.getRandomNumber(arrayObjectPlayers.length);
+        return arrayObjectPlayers[randomIndex];
+    }
+
+    return {
+        start
+    }
+})();
+
 function Player(playerName) {
     const name = playerName;
     const marker = generateMarker();
