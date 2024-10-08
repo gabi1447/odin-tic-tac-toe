a96a3413b4383bdd6e195d3cf613994fbd8a457f
Gabriel <gabrielpachecotec@gmail.com>
2024-10-08 06:57:55 +0100

    Finish Game module pattern and console version
    Added isThereWinner() to the game loop to check if there are any winning combinations.
    If there's any, we console.log() the winner and break out of the game loop.
    
    At the end of the game loop, we also change the turn of the player.

--- scripts/script.js
+++ scripts/script.js
@@ -59,18 +59,19 @@ const GameBoard = (function() {
 
 const Game = (function() {
     let currentPlayerTurn;
-    
+    let winner;
+
     function start() {
         const player1 = new Player(prompt("Name of player1:"));
         const player2 = new Player(prompt("Name of player2:"));
         const players = [player1, player2];
 
         currentPlayerTurn = whoStartsGame(players);
-        console.log(GameBoard.printBoardState());
+        GameBoard.printBoardState();
 
         // GAME LOOP
         while (true) {
-            console.log(`It's ${currentPlayerTurn.name} playing with the ${currentPlayerTurn.marker} marker.`);
+            console.log(`It's ${currentPlayerTurn.name}'s turn playing with the ${currentPlayerTurn.marker} marker.`);
             const selectedPosition = prompt(`Provide a position (e.g. 02 representing the row and column) for your ${currentPlayerTurn.marker} marker`);
 
             if (!isBoardCellFilled(selectedPosition)) {
@@ -80,8 +81,14 @@ const Game = (function() {
                 continue;
             }
 
-            // Check winner combinations
-            
+            if (isThereWinner()) {
+                const winnerObject = player1.marker === winner ? player1 : player2;
+                console.log(`${winnerObject.name} wins as ${winnerObject.marker}`);
+                break;
+            }
+
+            GameBoard.printBoardState();
+            currentPlayerTurn = currentPlayerTurn === player1 ? player2 : player1;
         }
     }
 
@@ -95,6 +102,40 @@ const Game = (function() {
     function whoStartsGame(arrayObjectPlayers) {
         const randomIndex = GeneralModule.getRandomNumber(arrayObjectPlayers.length);
         return arrayObjectPlayers[randomIndex];
+    }
+
+    function isThereWinner() {
+        const board = GameBoard.getBoardState();
+
+        // Possible winning combinations
+        const possibleWinningCombinations = {
+            row1: board[0],
+            row2: board[1],
+            row3: board[2],
+            col1: [board[0][0], board[1][0], board[2][0]],
+            col2: [board[0][1], board[1][1], board[2][1]],
+            col3: [board[0][2], board[1][2], board[2][2]],
+            diagonal1: [board[0][0], board[1][1], board[2][2]], 
+            diagonal2: [board[0][2], board[1][1], board[2][0]], 
+        }
+        return checkWinningCombination(possibleWinningCombinations);
+    }
+
+    function checkWinningCombination(combinations) {
+        for (key in combinations) {
+            if (combinations[key].every(cell => {
+                return cell === 'x';
+            })) {
+                winner = 'x';
+                return true;
+            } else if (combinations[key].every(cell => {
+                return cell === 'o';
+            })) {
+                winner = 'o';
+                return true;
+            }
+        }
+        return false;
     }
 
     return {
@@ -123,3 +164,5 @@ function Player(playerName) {
         marker
     }
 }
+
+Game.start();
